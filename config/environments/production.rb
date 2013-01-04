fb8ee0d34f93ac2eddec29bad031dd32321c2a39
touchdownkevin <kq7726@gmail.com>
2013-01-04 16:12:05 -0500

    Change web2 to web3 production.rb for image precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Web2::Application.configure do
+Web3::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
   # Code is not reloaded between requests
