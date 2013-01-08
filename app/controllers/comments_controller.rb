cef6b6e4fd958d1f1bad0ad4766595b7c6d3440d
touchdownkevin <kq7726@gmail.com>
2013-01-08 14:56:17 -0500

    test

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,4 +4,13 @@ class CommentsController < ApplicationController
     @comment=@post.comments.create!(params[:comment])
     redirect_to @post
   end
+
+  def index
+  	@post=Post.find(params[:post_id])
+  	@comments=@post.comments.all
+  	respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @posts }
+    end
+  end
 end
