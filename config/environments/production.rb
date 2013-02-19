79432f2bdbfcb44ade863cf6562180049addadcb
touchdownkevin <kq7726@gmail.com>
2013-02-18 22:38:12 -0500

    change assets compile to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ config.action_mailer.default_url_options = { :host => 'qshkevin.herokuapp.com' }
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
